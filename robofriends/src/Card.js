5680c6de45e097c5b77f4be2fb319cb74ddfaf4c
Thomas Voets <voets_thomas@hotmail.com>
2020-05-22 12:51:45 +0200

    Make the Card Component Dynamic by using Props

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img alt="" src="https://robohash.org/test?200x200" />
-      <h2>Jane Doe</h2>
-      <p>jane.doe@gmail.com</p>
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img alt="" src={`https://robohash.org/${props.id}?200x200`} />
+      <h2>{props.name}</h2>
+      <p>{props.email}</p>
     </div>
   );
 };
