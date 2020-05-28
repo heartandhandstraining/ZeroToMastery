1ee1987c7324be4d57fd2317760791241babd2db
Thomas Voets <voets_thomas@hotmail.com>
2020-05-28 09:32:50 +0200

    Sign In

--- SmartBrain/smart-brain-api/server.js
+++ SmartBrain/smart-brain-api/server.js
@@ -57,27 +57,52 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json('error logging in');
-  }
+  const { email, password } = req.body;
+
+  db.select('email', 'hash')
+    .from('login')
+    .where('email', '=', email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      if (isValid) {
+        return db
+          .select('*')
+          .from('users')
+          .where('email', '=', email)
+          .then((user) => res.json(user[0]))
+          .catch((err) => res.status(400).json('unable to get user'));
+      } else {
+        res.status(400).json('wrong credentials');
+      }
+    })
+    .catch((err) => res.status(400).json('wrong credentials'));
 });
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
-  db('users')
-    .returning('*')
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date(),
-    })
-    .then((user) => res.json(user))
-    .catch((err) => res.status(404).json('unable to register'));
+  const hash = bcrypt.hashSync(password);
+
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into('login')
+      .returning('email')
+      .then((loginEmail) => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => res.json(user));
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(404).json('unable to register'));
 });
 
 app.get('/profile/:id', (req, res) => {
